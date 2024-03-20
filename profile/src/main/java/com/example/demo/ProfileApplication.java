5350ebd7f965a0e28c8c7b007f5be2888c231ae1
Hem B K <baralhems12@gmail.com>
2024-03-19 21:30:21 -0500

    added servlet annotation

--- profile/src/main/java/com/example/demo/ProfileApplication.java
+++ profile/src/main/java/com/example/demo/ProfileApplication.java
@@ -2,12 +2,15 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
+@ServletComponentScan
 public class ProfileApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProfileApplication.class, args);
+		//Guthub desktop test
 	}
 
 }
